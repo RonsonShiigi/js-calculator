5256540d742ed86fc78733f90b2d6ed7393a913f
Ronson Shiigi <ronsonshiigi@Ronsons-MBP.home>
2018-12-13 20:56:33 -1000

    pau

--- calculator.js
+++ calculator.js
@@ -5,7 +5,75 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+var calculatorModule = function(){
+    
+var memory = 0;
+var total=0;
 
+var calculator= {
+    load: function(x){
+        if(typeof x === 'number'){
+            total=x;
+            return total
+        }else{
+            throw error 
+        }
+       
+    },
+    getTotal:function(){
+        return total
+    },
+    add:function(x){
+        if(typeof x === 'number'){
+            total += x;
+            return total
+        }else{
+            throw error
+        }
+    },
+    subtract:function(x){
+        if(typeof x === 'number'){
+            total -= x;
+            return total
+        }else{
+            throw error
+        }
+    },
+    multiply:function(x){
+        if(typeof x === 'number'){
+            total *= x;
+            return total
+        }else{
+            throw error
+        }
+    },
+    divide:function(x){
+        if(typeof x === 'number'){
+            total /= x;
+            return total
+        }else if(x=0){
+            throw error
+        }
+        else{
+            throw error
+        }
+    },
+    recallMemory: function(){
+        return memory
+    },
+    saveMemory:function(x){
+        memory=total;
+        return memory;
+    },
+    clearMemory:function(){
+        memory=0;
+        return memory
+    }
+};
+return calculator 
+
+
+};
 
   /**
    * sets the `total` to the number passed in
